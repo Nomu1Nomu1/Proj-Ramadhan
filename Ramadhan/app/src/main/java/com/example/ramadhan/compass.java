56530218985d31f0e4bf03dabeb42a8b1e3f9055
Nomu1Nomu1 <isaiah.tino@gmail.com>
2024-04-23 14:53:14 +0700

    Compass GPS

--- Ramadhan/app/src/main/java/com/example/ramadhan/compass.java
+++ Ramadhan/app/src/main/java/com/example/ramadhan/compass.java
@@ -56,12 +56,40 @@ public class compass implements SensorEventListener {
         setAzimuthFix(0);
     }
 
-    public void setListener(CompassListener){
-
+    public void setListener(CompassListener l){
+        listener = l;
     }
 
     @Override
     public void onSensorChanged(SensorEvent event) {
+        final float alpha = 0.97f;
+
+        synchronized (this){
+            if (event.sensor.getType() == Sensor.TYPE_ACCELEROMETER){
+
+                mGravity[0] = alpha * mGravity[0] + (1 - alpha) * event.values[0];
+                mGravity[1] = alpha * mGravity[1] + (1 - alpha) * event.values[1];
+                mGravity[2] = alpha * mGravity[2] + (1 - alpha) * event.values[2];
+            }
+
+            if (event.sensor.getType() == Sensor.TYPE_MAGNETIC_FIELD) {
+                mGeomagnetic[0] = alpha * mGeomagnetic[0] + (1 - alpha) * event.values[0];
+                mGeomagnetic[1] = alpha * mGeomagnetic[1] + (1 - alpha) * event.values[1];
+                mGeomagnetic[2] = alpha * mGeomagnetic[2] + (1 - alpha) * event.values[2];
+            }
+
+            boolean success = SensorManager.getRotationMatrix(R, I, mGravity, mGeomagnetic);
+            if (success) {
+                float orientation[] = new float[3];
+                SensorManager.getOrientation(R, orientation);
+                azimuth = (float) Math.toDegrees(orientation[0]);
+                azimuth = (azimuth + azimuthFix + 360) % 360;
+
+                if (listener != null) {
+                    listener.onNewAzimuth(azimuth);
+                }
+            }
+        }
 
     }
 
