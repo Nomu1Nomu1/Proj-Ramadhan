ee55fa57a4837600bb05be913f760788eb9458f5
Nomu1Nomu1 <isaiah.tino@gmail.com>
2024-04-23 12:01:07 +0700

    compass n loading file

--- Ramadhan/app/src/main/java/com/example/ramadhan/compass.java
+++ Ramadhan/app/src/main/java/com/example/ramadhan/compass.java
@@ -1,5 +1,68 @@
 package com.example.ramadhan;
 
-public class compass {
-    test
+import android.content.Context;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
+
+public class compass implements SensorEventListener {
+    private static final String TAG = "Compass";
+
+    public interface CompassListener {
+        void onNewAzimuth(float azimuth);
+    }
+
+    private CompassListener listener;
+    private SensorManager sensorManager;
+    private Sensor gsensor;
+    private Sensor msensor;
+
+    private float[] mGravity = new float[3];
+    private float[] mGeomagnetic = new float[3];
+    private float[] R = new float[9];
+    private float[] I = new float[9];
+
+    private float azimuth;
+    private float azimuthFix;
+
+    public compass(Context context) {
+        sensorManager = (SensorManager) context.getSystemService(Context.SENSOR_SERVICE);
+        gsensor = sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
+        msensor = sensorManager.getDefaultSensor(Sensor.TYPE_MAGNETIC_FIELD);
+    }
+
+//
+//    start compass to bind listener
+//
+    public void start() {
+        sensorManager.registerListener(this, gsensor, SensorManager.SENSOR_DELAY_GAME);
+        sensorManager.registerListener(this, msensor, SensorManager.SENSOR_DELAY_GAME);
+    }
+
+//
+//    stop compass listener
+//
+
+    public void stop() {
+        sensorManager.unregisterListener(this);
+    }
+
+    public void setAzimuthFix(float fix){
+        azimuthFix = fix;
+    }
+
+    public void resetAzimuthFix() {
+        setAzimuthFix(0);
+    }
+
+    @Override
+    public void onSensorChanged(SensorEvent event) {
+
+    }
+
+    @Override
+    public void onAccuracyChanged(Sensor sensor, int accuracy) {
+
+    }
 }
