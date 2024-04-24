11455847851b3c96d7f2b08234640c5ff744cd67
asus <rasyadelgiva@gmail.com>
2024-04-24 07:18:47 +0700

    yuuhu

--- Ramadhan/app/src/main/java/com/example/ramadhan/activityJadwalsholat.java
+++ Ramadhan/app/src/main/java/com/example/ramadhan/activityJadwalsholat.java
@@ -2,13 +2,43 @@ package com.example.ramadhan;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
 import android.os.Bundle;
+import android.os.Handler;
+import android.widget.TextView;
+
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+import kotlin.Suppress;
 
 public class activityJadwalsholat extends AppCompatActivity {
+    private TextView textClock, textDate;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_jadwalsholat);
+        textClock = findViewById(R.id.clock);
+        textDate = findViewById(R.id.date);
+
+        Handler handler = new Handler();
+        handler.post(new Runnable() {
+            @Override
+            public void run() {
+                Date date = new Date();
+                @SuppressLint("SimpleDateFormat")
+                DateFormat clockFormat = new SimpleDateFormat("HH:mm:ss");
+
+                @SuppressLint("SimpleDateFormat")
+                DateFormat dateFormat = new SimpleDateFormat("EEEE, dd MMMM yyyy");
+
+                textClock.setText(clockFormat.format(new Date()));
+                textDate.setText(dateFormat.format(new Date()));
+
+                handler.postDelayed(this,1000);
+            }
+        });
     }
 }
